9a4a0866299390da463b8c3ab23cf0e51a7c372a
simiso115 <simiso115@gmail.com>
2022-10-24 15:16:14 -0700

    Task 1

--- 0x05-pointer_arrays_string/0-reset_to_98.c
+++ 0x05-pointer_arrays_string/0-reset_to_98.c
@@ -1,32 +1,13 @@
 /**
- * _memset - a function that fills
- *         memory with a constant byte
+ * reset_to_98 - takes a pointer to an int parameter
+ *               and updates the value it points to 98
  *
- * @s: input pointer to char type
- *    represents the pointer the
- *    block of memory to fill
- * @b: input variable of char type
- *    represents the character to
- *    fill s
- * @n: unsigned int variable
- *    the number of bytes to be filled
+ * @n: int parameter
  *
- * Return: A pointer to the filled memory
- *         area @s
-*/
+ * Return: Nothing
+ */
 
-char *_memset(char *s, char b, unsigned int n)
+void reset_to_98(int *n)
 {
-	/**
-	 * declare an unsigned int
-	 * because we are storing a
-	 * value that will always be
-	 * non-negative (zero or positive)
-	*/
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		s[i] = b;
-
-	return (s);
+	*n = 98;
 }
