9ba3ac2c28387000bdac866f1f8a6991e8194b41
simiso115 <simiso115@gmail.com>
2022-10-24 15:25:07 -0700

    Task 1

--- 0x05-pointer_arrays_string/0-reset_to_98.c
+++ 0x05-pointer_arrays_string/0-reset_to_98.c
@@ -1,3 +1,6 @@
+#include<stdio.h>
+
+int main(){
 /**
  * reset_to_98 - takes a pointer to an int parameter
  *               and updates the value it points to 98
@@ -10,4 +13,6 @@
 void reset_to_98(int *n)
 {
 	*n = 98;
+}
+ return '0';
 }
